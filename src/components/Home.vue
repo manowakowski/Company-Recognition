3c5850266192564af5da6d36cdfa44d6881dbce6
manowakowski <nowakowski.maciej.nm@gmail.com>
2019-07-17 16:02:38 +0200

    base frame

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <main class="home">
+        <h1>Witaj w naszym centrum wsparcia</h1>
+        <p>
+            Jesteśmy tu aby pomóc. Proszę przeczytaj najpierw nasze <a> F.A.Q. </a>
+            Jeśli nie znalazleś odpowiedzi na swoje pytanie <a> wyślij je do nas </a>
+        </p>
+    </main>
+</template>
